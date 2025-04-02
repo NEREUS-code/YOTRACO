625c60930462ca0a4df3914b052e00e650e601cf
mohammed benyamna <mohammedbenyamna39@gmail.com>
2025-04-02 17:32:18 +0000

    fix the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yotraco",
-    version="0.2.3",  # Updated version
+    version="0.2.4",  # Updated version
     packages=find_packages(include=["YOTRACO*"], exclude=["images"]),  # Match the inclusion/exclusion from pyproject
     install_requires=[
         "torch",
